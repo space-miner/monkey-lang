7dc69324e8d721400f3be1331a36acf2707cfef4
tony <spambox@waifu.club>
2020-08-01 21:16:36 -0400

    finish read char method for lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -5,6 +5,15 @@ class Lexer:
     self.rpos = rpos
     self.ch = ch
 
+  def read_char(self):
+    l = self
+    if l.rpos >= len(l.inp):
+      l.ch = 0
+    else:
+      l.ch = l.inp[l.rpos]
+    l.pos = l.rpos
+    l.rpos += 1
+
 def new(inp):
   l = Lexer(inp)
   l.read_char()
